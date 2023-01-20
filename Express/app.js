c1cb2937f30e561a5d0b24c2fe6f57fdda0b9970
Martin-Paez <mpaez.ok@gmail.com>
2023-01-20 16:59:05 -0300

    Express no-view https template

--- Express/app.js
+++ Express/app.js
@@ -24,16 +24,4 @@ app.use('/', indexRouter);
 indexRouter.use(express.static(app.settings.public, {index: 'index.html'}));
 app.use('/users', usersRouter);
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  res.render('error', { title: 'Express' });
-});
-
 module.exports = app;
